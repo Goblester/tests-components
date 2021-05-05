964fb6ad68f6a2b592a12a2e1c4512ea31a7cbf3
Goblester <goblin829@gmail.com>
2021-05-05 14:40:51 +0300

    completed HW9

--- src/p2-homeworks/h9/Clock.tsx
+++ src/p2-homeworks/h9/Clock.tsx
@@ -3,29 +3,33 @@ import SuperButton from "../h4/common/c2-SuperButton/SuperButton";
 
 function Clock() {
     const [timerId, setTimerId] = useState<number>(0);
-    const [date, setDate] = useState<Date>();
+    const [date, setDate] = useState<Date>(new Date());
     const [show, setShow] = useState<boolean>(false);
 
     const stop = () => {
-        // stop
+        clearInterval(timerId);
     }
     const start = () => {
         stop();
         const id: number = window.setInterval(() => {
-            // setDate
+            setDate(new Date())
         }, 1000);
         setTimerId(id);
     }
 
     const onMouseEnter = () => {
-        // show
+        setShow(true);
     };
     const onMouseLeave = () => {
-        // close
+        setShow(false);
     };
 
-    const stringTime = "Time"; // fix with date
-    const stringDate = "Date"; // fix with date
+    const seconds = date.getSeconds()<10?'0'+date.getSeconds():date.getSeconds();
+    const minutes = date.getMinutes()<10?'0'+date.getMinutes():date.getMinutes();
+    const hours = date.getHours()<10?'0'+date.getHours():date.getHours();
+
+    const stringTime = `${hours}:${minutes}:${seconds}` // fix with date
+    const stringDate =`${date.getDay()}:${date.getMonth()}:${date.getFullYear()}`; // fix with date
 
     return (
         <div>
