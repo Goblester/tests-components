1d393e625ad41382268844b835b1a67bb895400b
Goblester <goblin829@gmail.com>
2021-07-31 02:25:54 +0300

    hash

--- src/index.tsx
+++ src/index.tsx
@@ -5,11 +5,14 @@ import App from './p1-main/m1-ui/u1-app/App'
 import * as serviceWorker from './serviceWorker'
 import {Provider} from 'react-redux';
 import store from './p2-homeworks/h10/bll/store';
+import {HashRouter} from 'react-router-dom'
 
 ReactDOM.render(
-    <Provider store={store}>
-        <App/>
-    </Provider>
+    <HashRouter>
+        <Provider store={store}>
+            <App/>
+        </Provider>
+    </HashRouter>
     ,
     document.getElementById('root')
 )
--- src/p2-homeworks/h5/HW5.tsx
+++ src/p2-homeworks/h5/HW5.tsx
@@ -1,20 +1,18 @@
 import React from 'react'
 import Header from './Header'
 import Routes from './Routes'
-import {HashRouter} from 'react-router-dom';
 import styles from './HW5.module.css';
 
 function HW5() {
     return (
         <div className={styles.container}>
             {/*в gh-pages лучше работает HashRouter*/}
-            <HashRouter>
 
-                <Header/>
 
-                <Routes/>
+            <Header/>
 
-            </HashRouter>
+            <Routes/>
+
         </div>
     )
 }
